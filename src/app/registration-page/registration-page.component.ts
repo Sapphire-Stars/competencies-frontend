ed2b20ba9880027ad0f74dcebb0c592f2e523305
pvsreenath <pvsreenath1610238@gmail.com>
2021-05-02 08:01:48 +0530

    second commit

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -9,23 +9,19 @@ import { Registration } from './registration';
   styleUrls: ['./registration-page.component.css']
 })
 export class RegistrationPageComponent implements OnInit {
-  info =false;
-  Error:any;
+  validator =false;
+ 
 
   userModel = new Registration('','','','');
   constructor(private es:EnrollmentService) { }
   onSubmit(){
     this.es.enroll(this.userModel)
     .subscribe(
-      data =>console.log('Success!',data),
+      data =>{
+        this.validator=false;
+        console.log('Success!',data)},
       error=>{
-        this.info=true;
-        let DataError= document.getElementById("email");
-        if(DataError){
-          console.log(DataError)
-          DataError.innerHTML="email already exist"
-        }
-        this.Error=error.error.error;
+        this.validator=true;
         console.log('Error!',error.error.error)}
     );
   }
